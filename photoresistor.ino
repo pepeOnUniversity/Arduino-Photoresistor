897ba9cba53918069517f424a50a2405ca09065e
pepeOnUniversity <contact.me.dothehung@gmail.com>
2025-11-23 05:09:37 +0700

    read state from pin digital

--- photoresistor.ino
+++ photoresistor.ino
@@ -1,11 +1,22 @@
+//declear
 int pinAnalog = A0;
+int sensorPin = 2;
 
+//setup
 void setup() {
+  pinMode(sensorPin, 2);
   Serial.begin(9600);
 }
 
+//run
 void loop() {
-  int valueAnalog = analogRead(pinAnalog);
-  Serial.println(valueAnalog);
-  delay(200);
+  //read value state from pin 2
+  int state = digitalRead(sensorPin);
+  //read value adc from A0
+  int adc = analogRead(pinAnalog);
+  Serial.print("Value of ADC: ");
+  Serial.println(adc);
+  Serial.print("Value of state: ");
+  Serial.println(state);
+  delay(800);
 }
